5406e56cb15fcfbd9e6e0d4c14eebd49984ae1ab
EatFan <eatfan0921@163.com>
2025-05-01 22:49:26 +0800

    Main

---
+++ src/main/java/cn/fan/snake/Main.java
@@ -0,0 +1,12 @@
+package cn.fan.snake;
+
+
+public class Main {
+    public static void main(String[] args) {
+        GameManager gameManager = new GameManager(30,60);
+        gameManager.run();
+    }
+
+
+
+}
