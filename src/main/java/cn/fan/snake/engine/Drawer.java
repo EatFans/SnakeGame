a53e11c014cc89b40ee57d7ffb21832bdf21735c
EatFan <eatfan0921@163.com>
2025-05-01 10:46:21 +0800

    渲染器

---
+++ src/main/java/cn/fan/snake/engine/Drawer.java
@@ -0,0 +1,42 @@
+package cn.fan.snake.engine;
+
+import cn.fan.snake.engine.ansi.Terminal;
+
+/**
+ * 渲染类
+ *
+ * @author Fan
+ */
+public class Drawer {
+    public Drawer(){
+
+    }
+
+    /**
+     * 绘制内容，在绘制到该格结束重置终端样式
+     * @param row 行
+     * @param col 列
+     * @param color 颜色
+     * @param bgColor 背景颜色
+     * @param content 内容
+     */
+    public void draw(int row, int col,String color,String bgColor, String content){
+        Terminal.moveTo(row,col);
+        System.out.print(color + bgColor + content);
+        Terminal.reset();
+    }
+
+
+    /**
+     * 安全绘制，保证不会破坏已经绘制好的内容
+     * @param row 行
+     * @param col 列
+     * @param color 前景色
+     * @param bgColor 背景色
+     * @param content 内容
+     */
+    public void safeDraw(int row, int col, String color, String bgColor, String content) {
+        Terminal.moveTo(row,col);
+        System.out.print(color + bgColor + content);
+    }
+}
