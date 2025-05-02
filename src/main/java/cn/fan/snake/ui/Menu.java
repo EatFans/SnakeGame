8e7482610f25f5f9ffab5ff8f62339fb58e4ca0f
EatFan <eatfan0921@163.com>
2025-05-02 15:39:57 +0800

    优化Menu

--- src/main/java/cn/fan/snake/ui/Menu.java
+++ src/main/java/cn/fan/snake/ui/Menu.java
@@ -34,9 +34,12 @@ public class Menu extends UI{
         // 菜单文本绘制
         drawer.drawText(realRow+2,3*2,ForeColor.WHITE,"使用w、s、a、d控制，1、2选择按钮");
         drawer.drawText(realRow+3,3*2,ForeColor.WHITE,"enter确定选择");
-        drawer.drawText(realRow+3,22*2,ForeColor.YELLOW,"得分：");
-        drawer.drawText(realRow+5,22*2,ForeColor.GREEN,"长度：");
-        drawer.drawText(realRow+7,22*2,ForeColor.RED,"FPS：");
+        drawer.drawText(realRow+2,22*2,ForeColor.YELLOW,"得分：");
+        drawer.drawText(realRow+4,22*2,ForeColor.GREEN,"长度：");
+        drawer.drawText(realRow+6,22*2,ForeColor.PURPLE,"输入：");
+        drawer.drawText(realRow+8,22*2,ForeColor.RED,"FPS：");
+
+
     }
 
 
