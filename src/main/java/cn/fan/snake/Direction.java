ddd523724300002e55d38ca24b9642721cc6fa8f
EatFan <eatfan0921@163.com>
2025-05-02 20:20:16 +0800

    方向枚举

---
+++ src/main/java/cn/fan/snake/Direction.java
@@ -0,0 +1,22 @@
+package cn.fan.snake;
+
+/**
+ * 方向枚举
+ *
+ * @author Fan
+ */
+public enum Direction {
+    UP("w"),
+    DOWN("s"),
+    LEFT("a"),
+    RIGHT("d");
+
+    private final String code;
+    Direction(String code){
+        this.code = code;
+    }
+
+    public String getCode(){
+        return code;
+    }
+}
