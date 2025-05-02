6f8f1f6f94384d65d1eef1e357bfd221d553123f
EatFan <eatfan0921@163.com>
2025-05-02 15:39:47 +0800

    修改前景色

--- src/main/java/cn/fan/snake/engine/ansi/ForeColor.java
+++ src/main/java/cn/fan/snake/engine/ansi/ForeColor.java
@@ -12,6 +12,6 @@ public class ForeColor {
     public static final String YELLOW = "\u001B[33m";
     public static final String BLUE = "\u001B[34m";
     public static final String PURPLE = "\u001B[35m";
-    public static final String CYAN = "\u001B[36m";
+    public static final String CYAN = "\u001B[90m";
     public static final String WHITE = "\u001B[37m";
 }
